abd6a09555aa4380e470bbed2873e0143fee80f7
Sergey Gorokhov <sergey.login@gmail.com>
2020-01-12 00:13:04 +0300

    Add save log data to MySQL database

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask import render_template, request, escape
 from vsearch import search4letters
+import mysql.connector
 
+dbconfig = {'host': '127.0.0.1', 'user': 'vsearch', 'password': 'vsearchpasswd', 'database': 'vsearchlogDB', }
+conn = mysql.connector.connect(**dbconfig)
+cursor = conn.cursor()
+_SQL = '''insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)'''
 
 app = Flask(__name__)
 
@@ -44,6 +49,9 @@ def do_search() -> 'html':
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
         print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res,))
+    conn.commit()
 
 
 if __name__ == '__main__':
