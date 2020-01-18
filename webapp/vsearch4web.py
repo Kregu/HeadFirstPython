d239bdf8ed34bedeeefa022fc54e6fa3ee8d173a
Sergey Gorokhov <sergey.login@gmail.com>
2020-01-18 18:15:19 +0300

    Remove commented string

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -43,11 +43,6 @@ def do_search() -> 'html':
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    # with open('vsearch.log', 'a') as log:
-    #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-
-    # cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res,))
-    # conn.commit()
 
     with UseDatabase(app.config['dbconfig']) as cursor:
         _SQL = '''insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)'''
