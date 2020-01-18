569f129501e56f42e503b70c583e4f699efa7d33
Sergey Gorokhov <sergey.login@gmail.com>
2020-01-18 18:14:14 +0300

    Use context to save log

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,15 +1,11 @@
 from flask import Flask
 from flask import render_template, request, escape
 from vsearch import search4letters
-import mysql.connector
+from DBcm import UseDatabase
 
-dbconfig = {'host': '127.0.0.1', 'user': 'vsearch', 'password': 'vsearchpasswd', 'database': 'vsearchlogDB', }
-conn = mysql.connector.connect(**dbconfig)
-cursor = conn.cursor()
-_SQL = '''insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)'''
 
 app = Flask(__name__)
-
+app.config['dbconfig'] = {'host': '127.0.0.1', 'user': 'vsearch', 'password': 'vsearchpasswd', 'database': 'vsearchlogDB', }
 
 @app.route('/')
 @app.route('/entry')
@@ -47,11 +43,20 @@ def do_search() -> 'html':
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    # with open('vsearch.log', 'a') as log:
+    #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
-    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res,))
-    conn.commit()
+    # cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res,))
+    # conn.commit()
+
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = '''insert into log (phrase, letters, ip, browser_string, results) values (%s, %s, %s, %s, %s)'''
+        cursor.execute(_SQL, (
+                            req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            req.user_agent.browser,
+                            res,))
 
 
 if __name__ == '__main__':
