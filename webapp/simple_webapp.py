0900e9a9a27202e8f4b8b2c8edfbb181ae50fa00
Sergey Gorokhov <sergey.login@gmail.com>
2020-01-19 16:41:01 +0300

    Add decorator for check logged in user

--- webapp/simple_webapp.py
+++ webapp/simple_webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -7,14 +8,17 @@ def hello():
     return 'Hello from the simple web app.'
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
     return 'This is page 1.'
 
 @app.route('/page2')
+@check_logged_in
 def page2() -> str:
     return 'This is page 2.'
 
 @app.route('/page3')
+@check_logged_in
 def page3() -> str:
     return 'This is page 3.'
 
@@ -27,12 +31,6 @@ def do_login() -> str:
 def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out'
-
-@app.route('/status')
-def check_status() -> str:
-    if 'logged_in' in session:
-        return 'You are currently logged in.'
-    return 'You are NOT logged in'
 
 app.secret_key = 'MySecretKeyIs1234'
 
